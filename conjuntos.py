90fcbfa0dd60291f166f517a870bb6408113fa70
leomatheuss <leo.mth2@gmail.com>
2020-04-14 19:35:44 -0300

    aha

---
+++ conjuntos.py
@@ -0,0 +1,31 @@
+#no python conjuntos são conhecidos como sets
+
+'''
+assim como namatememática
+
+-sets nao possuem valores duplicados
+-'' ''           ''valores ordenados
+- elementos sao acessados via indice, ou seja, conjuntos nao sao indexados
+
+conjuntos sao bons para se utilizar qdo precisamos armazenar elementos, mas nao nos importamos com a ordenacao deles
+qdo nao precisamos se preocupar com keys, valores e itens duplicados    
+
+os sets sao referenciados com {}
+
+diferença entre conjunto e maps/dics 
+
+um dicionário tem {chave: valor} um conjunto apenas {valor}
+
+'''
+
+#definindo um conjunto
+#forma 1
+
+s = set({1,2,3,4,4,5,5,6,7})
+#ao criar um conjunto, caso seja adcionado um valor já existente, o mesmo será ignorado
+#sem gerar erros e não fará parte do conjunto
+
+#forma2 (comun)
+
+s = {1,2,3,4,5,5,6,6,6,}
+
